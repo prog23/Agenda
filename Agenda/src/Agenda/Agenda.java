46e4b1c8c9a1808518bfd67a549bd3e8a7fe7039
prog23 <tamavilpet@gmail.com>
2019-04-11 13:07:56 +0200

    Está como quien dice hecho

--- Agenda/src/Agenda/Agenda.java
+++ Agenda/src/Agenda/Agenda.java
@@ -21,84 +21,14 @@ public class Agenda extends TreeMap<String, String>{
 	
 	public static void main(String[] args) throws IOException {			
 			
-		escribir("agenda.dat", leerConScanner("Agenda.txt", "utf-8"));
+		escribir("agenda.dat", leerConScanner("Agenda.txt", "utf-8"));	
 		
 		
-		
-		/*do {
-			System.out.print("> ");
-			Scanner s = new Scanner(in.readLine());
-			int estado = 0;
-			String token;
-			String nombre = null;
-			while (estado != 5) {
-				switch (estado) {
-				case 0:
-					try {
-						token = s.skip("fin|buscar|[a-zA-ZáéíóúÁÉÍÓÚ]+\\s+([a-zA-ZáéíóúÁÉÍÓÚ]+\\s+)*[a-zA-ZáéíóúÁÉÍÓÚ]+|[a-zA-ZáéíóúÁÉÍÓÚ]+").match().group();
-						if (token.equals("fin"))
-							estado = 5;
-						else if (token.equals("buscar"))
-							estado = 2;
-						else {
-							nombre = token;
-							estado = 1;
-						}
-					} catch (NoSuchElementException e) {
-						System.out.println("Se esperaba 'buscar' o 'fin' o un nombre");
-						estado = 5;
-					}
-					break;
-				case 1:
-					try {
-						s.skip("-");
-						estado = 3;
-					}catch (NoSuchElementException e) {
-						System.out.println("Se esperaba '-'");
-						estado = 5;
-					}
-					break;
-				case 2:
-					try {
-						s.skip(":");
-						estado = 4;
-					}catch (NoSuchElementException e) {
-						System.out.println("Se esperaba ':'");
-						estado = 5;
-					}
-					break;
-				case 3:
-					try {
-						token = s.skip("\\d{9}").match().group();
-						agenda.put(nombre, token);
-						estado = 5;
-					}catch (NoSuchElementException e) {
-						System.out.println("Se esperaba un teléfono");
-						estado = 5;
-					}
-					break;
-				case 4:
-					try {
-						token = s.skip("[a-zA-ZáéíóúÁÉÍÓÚ]+\\s+([a-zA-ZáéíóúÁÉÍÓÚ]+\\s+)*[a-zA-ZáéíóúÁÉÍÓÚ]+|[a-zA-ZáéíóúÁÉÍÓÚ]+").match().group();
-						String telefono = agenda.get(token);
-						if (telefono != null)
-							System.out.println(token + " -> " + telefono);
-						else
-							System.out.println(token + " no se encuentra en la agenda");
-						estado = 5;
-					} catch (NoSuchElementException e) {
-						System.out.println("Se esperaba un nombre");
-						estado = 5;
-					}
-					break;
-				}
-			}
-		} while (!fin);*/
 	}
 	
 	static Map<String, Integer> leerConScanner(String fichero, String codigo) throws IOException{
 		Map<String, String> agenda = new HashMap<>();
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); //Con esto es para las líneas de texto de texto
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		String linea;
 		Map<String, Integer> map = new TreeMap<>();
 		boolean fin = false;
@@ -182,7 +112,4 @@ public class Agenda extends TreeMap<String, String>{
 
 }	
 		
-	/* public class Agenda extends TreeMap<String, String>{
-	 	public String Ejecutar(String comando){....}
-	 	
-	 }*/
+	
